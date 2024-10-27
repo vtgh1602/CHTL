51454fb01fe32afae575b9c2175e3c43fc82a679
hoangvu109 <vuviethoang0@gmail.com>
2024-10-27 15:22:40 +0700

    create first entity

---
+++ src/edu/iuh/fit/cs/entities/Employee.java
@@ -0,0 +1,25 @@
+/*
+ * @ (#) Employee.java       1.0     27/10/2024
+ *
+ * Copyright (c) 2024 IUH. All rights reserved.
+ */
+
+package edu.iuh.fit.cs.entities;
+/*
+ * @decription:
+ * @author: Hoang Vu
+ * @date:   27/10/2024
+ * @version: 1.0
+ */
+
+import java.sql.Date;
+
+public class Employee {
+    private String id;
+    private String name;
+    private String phone;
+    private String address;
+    private String email;
+    private Date hiredDate;
+    private boolean status;
+}
